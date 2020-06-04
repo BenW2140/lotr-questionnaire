c6b9461cd8be1db6fcb759aa20b22247e5ac3d0a
Ben White <bwhite2140@outlook.com>
2020-06-04 15:52:06 -0700

    add base javascript functions
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
   $("#questionnaire").submit(function(event) {
     event.preventDefault();
+    const question1 = $("input:radio[name=question1]:checked").val();
+    const question2 = $("input:radio[name=question2]:checked").val();
+    const question3 = $("input:radio[name=question3]:checked").val();
   });
 });
